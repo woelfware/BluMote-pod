089c8d85a78e63f110a344df1593e698cb6e5afa
ken hutchins <ken@woelfware.com>
2011-04-18 21:19:26 -0500

    I believe these are matts changes

--- sw/BluMote/src/buffer.c
+++ sw/BluMote/src/buffer.c
@@ -9,15 +9,17 @@ void buf_init(struct circular_buffer *pQue, volatile uint8_t *buf, uint8_t size)
 {
 	pQue->buf = buf;
 	pQue->size = size;
-	pQue->writePointer = pQue->readPointer = 0;
+	pQue->cnt = 0;
+	pQue->wr_ptr = pQue->rd_ptr = 0;
 }
 
 bool buf_enque(struct circular_buffer *que, uint8_t k)
 {
 	bool isFull = buf_full(que);
 	if (!isFull) {
-		que->buf[que->writePointer] = k;
-		que->writePointer = (que->writePointer + 1) & (que->size - 1);
+		que->buf[que->wr_ptr] = k;
+		que->wr_ptr = (que->wr_ptr + 1) & (que->size - 1);
+		que->cnt++;
 	}
 	return isFull;
 }
@@ -27,9 +29,10 @@ bool buf_deque(struct circular_buffer *que, uint8_t *pK)
 	bool isEmpty = buf_empty(que);
 	if (!isEmpty) {
 		if (pK) {
-			*pK = que->buf[que->readPointer];
+			*pK = que->buf[que->rd_ptr];
 		}
-		que->readPointer = (que->readPointer + 1) & (que->size - 1);
+		que->rd_ptr = (que->rd_ptr + 1) & (que->size - 1);
+		que->cnt--;
 	}
 	return isEmpty;
 }
